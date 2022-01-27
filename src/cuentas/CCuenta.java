7a16b4486ae3108c106c06cd193f761e831624ba
Trabajo <Trabajo@DESKTOP-8UT1CCL>
2022-01-27 23:35:48 +0100

    2.0
    Subimos la versión 2.0 con Javadoc incluido

--- src/cuentas/CCuenta.java
+++ src/cuentas/CCuenta.java
@@ -1,5 +1,12 @@
 package cuentas;
 
+/**
+ * Esta clase CCuenta la utilizamos para operar con el objeto CCuenta. Contiene todos los métodos necesarios
+ * @author Javier Pego Paz
+ * @version 2.0
+ * @since 1.0
+ * 
+ */
 public class CCuenta {
 
     private String nombre;
@@ -8,6 +15,7 @@ public class CCuenta {
     private double tipoInterés;
     
     /**
+     * Este es el método get del atributo privado nombre
      * @return the nombre
      */
     public String getNombre() {
@@ -15,6 +23,7 @@ public class CCuenta {
     }
 
     /**
+     * Este es el método set del atributo privado nombre
      * @param nombre the nombre to set
      */
     public void setNombre(String nombre) {
@@ -22,6 +31,7 @@ public class CCuenta {
     }
 
     /**
+     * Este es el método get del atributo privado cuenta
      * @return the cuenta
      */
     public String getCuenta() {
@@ -29,6 +39,7 @@ public class CCuenta {
     }
 
     /**
+     * Este es el método set del atributo privadocuenta
      * @param cuenta the cuenta to set
      */
     public void setCuenta(String cuenta) {
@@ -36,13 +47,15 @@ public class CCuenta {
     }
 
     /**
+     * Este es el método get del atributo privado saldo
      * @return the saldo
      */
     public double getSaldo() {
         return saldo;
     }
 
-    /**
+    /** 
+     * Este es el método set del atributo privado saldo
      * @param saldo the saldo to set
      */
     public void setSaldo(double saldo) {
@@ -50,6 +63,7 @@ public class CCuenta {
     }
 
     /**
+     * Este es el método get del atributo privado tipo de interés
      * @return the tipoInterés
      */
     public double getTipoInterés() {
@@ -57,36 +71,51 @@ public class CCuenta {
     }
 
     /**
+     * Este es el método set del atributo privado tipoInterés
      * @param tipoInterés the tipoInterés to set
      */
     public void setTipoInterés(double tipoInterés) {
         this.tipoInterés = tipoInterés;
     }
 
-
-    public CCuenta()
-    {
-    }
-
+    /**
+     * Este es el método constructor del objeto cuenta. Se le pasan los siguientes parámetros:
+     * @param nom el nombre del dueño de la cuenta
+     * @param cue la cuenta completa del banco
+     * @param sal el saldo en cuenta
+     * @param tipo el tipo de cuenta
+     */
     public CCuenta(String nom, String cue, double sal, double tipo)
     {
         nombre =nom;
         cuenta=cue;
         saldo=sal;
     }
-
+    /**
+     * Este método nos devuelve la cantidad de saldo en cuenta
+     * @return Devuelve la cantidad de saldo 
+     */
     public double estado()
     {
         return getSaldo();
     }
 
+    /**
+     * Este método ingresa una cantidad en la cuenta
+     * @param cantidad es el monto a ingresar
+     * @throws Exception Si se intenta ingresar una cantidad negativa devuelve el error. "No se puede ingresar una cantidad negativa"
+     */
     public void ingresar(double cantidad) throws Exception
     {
         if (cantidad<0)
             throw new Exception("No se puede ingresar una cantidad negativa");
         setSaldo(getSaldo() + cantidad);
     }
-
+    /**
+     * Este método retira saldo de la cuenta
+     * @param cantidad la cantidad a retirar (double)
+     * @throws Exception  Si no hay suficiente saldo devuelve el error "no hay suficiente saldo"
+     */
     public void retirar(double cantidad) throws Exception
     {
         if (cantidad <= 0)
